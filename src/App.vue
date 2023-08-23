3e6d425469b394d720b20bb1d667bbf50a95fbc8
Mouad1502 <109124876+Mouad1502@users.noreply.github.com>
2023-08-23 14:21:10 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col min-h-screen font-Roboto bg-wheather-primary">
+    <SiteNavigation/>
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+import SiteNavigation from './components/SiteNavigation.vue';
+</script>
